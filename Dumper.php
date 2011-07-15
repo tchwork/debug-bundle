a570f9ac626697faa09fcdcbedc7b1ec09c0627a
Nicolas Grekas <nicolas.grekas@gmail.com>
2011-07-15 13:08:41 +0200

    * license to LGPL

--- Dumper.php
+++ Dumper.php
@@ -1,4 +1,15 @@
-<?php
+<?php /****************** vi: set fenc=utf-8 ts=4 sw=4 et: *****************
+ *
+ *   Copyright : (C) 2011 Nicolas Grekas. All rights reserved.
+ *   Email     : p@tchwork.org
+ *   License   : http://www.gnu.org/licenses/lgpl.txt GNU/LGPL
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ ***************************************************************************/
 
 namespace Patchwork\PHP;
 
