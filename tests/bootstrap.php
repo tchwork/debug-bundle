7a6c55dd5d11c3339896fa473bee6a01e1f15fa8
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-12-30 12:15:14 +0100

    fixing tests and related cleanups

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,4 +7,5 @@ require_once $dir . '/class/Patchwork/PHP/Dumper.php';
 require_once $dir . '/class/Patchwork/PHP/Dumper/Caster.php';
 require_once $dir . '/class/Patchwork/PHP/JsonDumper.php';
 require_once $dir . '/class/Patchwork/PHP/Logger.php';
-require_once $dir . '/class/Patchwork/PHP/ErrorHandler.php';
+require_once $dir . '/class/Patchwork/PHP/ThrowingErrorHandler.php';
+require_once $dir . '/class/Patchwork/PHP/InDepthErrorHandler.php';
