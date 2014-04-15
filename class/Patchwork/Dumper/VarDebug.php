d4bd37e7cdc46193516b5e309eec4c61b3657440
Nicolas Grekas <nicolas.grekas@gmail.com>
2014-04-15 11:33:21 +0200

    default to HtmlDumper when not in cli SAPI

--- class/Patchwork/Dumper/VarDebug.php
+++ class/Patchwork/Dumper/VarDebug.php
@@ -4,6 +4,7 @@ namespace Patchwork\Dumper;
 
 use Patchwork\Dumper\Collector\PhpCollector;
 use Patchwork\Dumper\Dumper\CliDumper;
+use Patchwork\Dumper\Dumper\HtmlDumper;
 
 /**
  * @author Nicolas Grekas <p@tchwork.com>
@@ -17,7 +18,7 @@ class VarDebug
     {
         if (!isset(self::$handler)) {
             $collector = new PhpCollector();
-            $dumper = new CliDumper();
+            $dumper = 'cli' === PHP_SAPI ? new CliDumper() : new HtmlDumper();
             self::$handler = function ($var) use ($collector, $dumper) {
                 $dumper->dump($collector->collect($var));
             };
